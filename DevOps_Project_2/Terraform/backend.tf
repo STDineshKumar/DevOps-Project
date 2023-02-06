d082f51efdd7128160786e70396eb62e4a8919eb
STDineshKumar <49930552+STDineshKumar@users.noreply.github.com>
2023-02-06 17:52:28 +0530

    Update backend.tf

--- DevOps_Project_2/Terraform/backend.tf
+++ DevOps_Project_2/Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "euran-terraform-statefile"
+    bucket = "euran-dineshterraform-statefile"
     key = "server_name/statefile"
     region = "ap-south-1"
   }
